8b10ee55dc7429cfacd4c7be5a68d8241c73d96b
maxwell-1999 <maxwell@buffer.finance>
2023-11-14 21:04:47 +0530

    WIP

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -53,7 +53,7 @@ const Counter: React.FC<Props> = ({ smartAccount, provider }) => {
       };
       console.log(`1[deb-txn]Counter-const: `, tx1);
 
-      let partialUserOp = await smartAccount.buildUserOp([tx1]);
+      let partialUserOp = await smartAccount.buildUserOp([tx1, tx1, tx1, tx1]);
       console.log(`2[deb-txn]Counter-partialUserOp: `, partialUserOp);
 
       const biconomyPaymaster =
